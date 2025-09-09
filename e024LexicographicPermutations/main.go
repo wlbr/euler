8fe4dca1a5c1d1f117bc918359a8d1359a52b32e
Michael Wolber <mwolber@gmx.de>
2025-09-09 17:25:12 +0200

    Added exercise 24.

--- e024LexicographicPermutations/main.go
+++ e024LexicographicPermutations/main.go
@@ -13,13 +13,60 @@ The lexicographic permutations of 0, 1 and 2 are:
 
 What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 
+Answer:  2783915460
 */
 
 package main
 
-func permutate(nums []int, k int) {
+import (
+	"fmt"
+	"sort"
+)
 
+func mergeArrayToInt(arr []int) int {
+	var result int
+	var power int = 1
+	for i := len(arr) - 1; i >= 0; i-- {
+		result += arr[i] * power
+		power *= 10
+	}
+	return result
+}
+
+// createAllPermutations generates all permutations of a slice of integers.
+// It uses a recursive approach to generate the permutations.
+func createAllPermutations(nums []int) []int {
+	var result []int
+	var generate func(arr []int, n int)
+
+	generate = func(arr []int, n int) {
+		if n == 1 {
+			// We have a full permutation, add a copy to the result.
+			tmp := make([]int, len(arr))
+			copy(tmp, arr)
+			result = append(result, mergeArrayToInt(tmp))
+			return
+		}
+
+		for i := 0; i < n; i++ {
+			generate(arr, n-1)
+			// Swap elements to generate the next permutation.
+			if n%2 == 1 {
+				arr[0], arr[n-1] = arr[n-1], arr[0]
+			} else {
+				arr[i], arr[n-1] = arr[n-1], arr[i]
+			}
+		}
+	}
+
+	generate(nums, len(nums))
+	sort.Ints(result)
+	return result
 }
 
 func main() {
+	digits := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	permutations := createAllPermutations(digits)
+
+	fmt.Println(permutations[999999])
 }
