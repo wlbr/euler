153e1211966a936285c37b8685b8772e4736c0fd
Michael Wolber <mwolber@gmx.de>
2025-09-09 17:41:58 +0200

    Added exercise 25.

--- e025-1000DigitFibonacciNumber/main.go
+++ e025-1000DigitFibonacciNumber/main.go
@@ -21,7 +21,7 @@ Hence the first 12 terms will be:
 	F11 = 89
 	F12 = 144
 
-The $12$th term, F12, is the first term to contain three digits.
+The 12th term, F12, is the first term to contain three digits.
 
 What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
 
